1413ee396ad224fec6bb1668481b056e286a1795
Leif Ringstad <leifcr@gmail.com>
2025-08-29 13:49:05 +0200

    Update selenium webdriver fetching

--- update_webdrivers.rb
+++ update_webdrivers.rb
@@ -1,3 +1,18 @@
-require 'webdrivers'
-Webdrivers::Chromedriver.update
-Webdrivers::Geckodriver.update
+# Prefetch Selenium Manager drivers (Chrome & Firefox) so first test is fast.
+require "selenium-webdriver"
+
+def prefetch(browser)
+  case browser
+  when :chrome
+    opts = Selenium::WebDriver::Options.chrome(args: %w[--headless=new --disable-gpu])
+  when :firefox
+    opts = Selenium::WebDriver::Options.firefox(args: %w[-headless])
+  end
+  driver = Selenium::WebDriver.for(browser, options: opts)
+  driver.quit
+  puts "#{browser} driver ready"
+rescue => e
+  warn "#{browser} prefetch failed: #{e.class}: #{e.message}"
+end
+
+[:chrome, :firefox].each { |b| prefetch(b) }
